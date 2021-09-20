39fdb50c705ed813771a52d4c496fa56825ca534
gomesuit <9404411+gomesuit@users.noreply.github.com>
2021-09-20 12:53:21 +0900

    add terraform sample code

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
